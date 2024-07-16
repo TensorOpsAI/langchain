d06008c725f198695c5ddc194c31b96a2796d3c6
Diogo Goncalves <diogo.goncalves@tensorops.ai>
2024-07-16 16:59:27 +0100

    feat: added integration tests

---
+++ libs/community/tests/integration_tests/chat_models/test_llmstudio.py
@@ -0,0 +1,70 @@
+"""Test Anthropic API wrapper."""
+
+from typing import List
+
+from langchain_core.callbacks import (
+    CallbackManager,
+)
+from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.outputs import ChatGeneration, LLMResult
+
+from langchain_community.chat_models.llmstudio import ChatLLMStudio
+from tests.unit_tests.callbacks.fake_callback_handler import FakeCallbackHandler
+
+
+def test_llmstudio_call() -> None:
+    """Test valid call to llmstudio."""
+    chat = ChatLLMStudio(  # type: ignore[call-arg]
+        model="test",
+    )
+    message = HumanMessage(content="Hello")
+    response = chat.invoke([message])
+    assert isinstance(response, AIMessage)
+    assert isinstance(response.content, str)
+
+
+def test_llmstudio_generate() -> None:
+    """Test generate method of anthropic."""
+    chat = ChatLLMStudio(model="test")  # type: ignore[call-arg]
+    chat_messages: List[List[BaseMessage]] = [
+        [HumanMessage(content="How many toes do dogs have?")]
+    ]
+    messages_copy = [messages.copy() for messages in chat_messages]
+    result: LLMResult = chat.generate(chat_messages)
+    assert isinstance(result, LLMResult)
+    for response in result.generations[0]:
+        assert isinstance(response, ChatGeneration)
+        assert isinstance(response.text, str)
+        assert response.text == response.message.content
+    assert chat_messages == messages_copy
+
+
+def test_llmstudio_streaming() -> None:
+    """Test streaming tokens from anthropic."""
+    chat = ChatLLMStudio(model="test", streaming=True)  # type: ignore[call-arg]
+    message = HumanMessage(content="Hello")
+    response = chat.invoke([message])
+    assert isinstance(response, AIMessage)
+    assert isinstance(response.content, str)
+
+
+def test_llmstudio_streaming_callback() -> None:
+    """Test that streaming correctly invokes on_llm_new_token callback."""
+    callback_handler = FakeCallbackHandler()
+    callback_manager = CallbackManager([callback_handler])
+    chat = ChatLLMStudio(  # type: ignore[call-arg]
+        model="test",
+        streaming=True,
+        callback_manager=callback_manager,
+        verbose=True,
+    )
+    message = HumanMessage(content="Write me a sentence with 10 words.")
+    chat.invoke([message])
+    assert callback_handler.llm_streams > 1
+
+def test_llmstudio_function_calling() -> None:
+    """Test function calling of llmstudio."""
+    raise NotImplementedError("Test not implemented")
+
+
+
